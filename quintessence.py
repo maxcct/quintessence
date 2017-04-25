ee4e09243fa8d5ed2433da2b53c24bcfc740a198
MaxCCT <maxcct@cantab.net>
2017-04-24 19:15:52 -0600

    Adds click event handler

--- quintessence.py
+++ quintessence.py
@@ -18,9 +18,17 @@ class GameBoard(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.canvas = tk.Canvas(self, borderwidth=0, highlightthickness=0,
                                 width=canvas_width, height=canvas_height, background="bisque")
-        self.canvas.pack(side="top", fill="both", expand=True, padx=2, pady=2)
 
         self.canvas.bind("<Configure>", self.refresh)
+
+        self.canvas.pack(side="top", fill="both", expand=True, padx=2, pady=2)
+
+    def callback(self, event, board):
+        print event.x, event.y
+        pieces = board.p1_pieces + board.p2_pieces
+        for piece in pieces:
+            if abs(piece.column - event.x) < 20 and abs(piece.row - event.y) < 20:
+                print piece.name
 
     def refresh(self, event):
         xsize = int((event.width-1) / self.columns)
@@ -47,30 +55,28 @@ class GameBoard(tk.Frame):
 
 class Piece(object):
     def __init__(self, player, name, element, board):
-        starting_positions = {"p1_air": 4, "p1_fire": 9, "p1_water": 14,
-                              "p1_earth": 19, "p2_air": 19, "p2_fire": 14,
-                              "p2_water": 9, "p2_earth": 4}
+        starting_positions = {"p1_air": 5.48, "p1_fire": 2.598, "p1_water": 1.7,
+                              "p1_earth": 1.265, "p2_air": 1.265, "p2_fire": 1.7,
+                              "p2_water": 2.598, "p2_earth": 5.48}
         self.player = player
         self.name = name
         self.element = element
         self.board = board
         self.image = tk.PhotoImage(file="%s.gif" % element)
         self.board.canvas.create_image(0,0, image=self.image, tags=(name, "piece"), anchor="c")
-        self.row = starting_positions[name]
+        self.row = (self.board.size * self.board.rows) / starting_positions[name]
         if player == "one":
-            self.column = 0
+            self.column = int(self.board.size / 2)
             self.board.p1_pieces.append(self) 
         else:
-            self.column = 23
+            self.column = (22.4 * self.board.size) + int(self.board.size / 2)
             self.board.p2_pieces.append(self)   
         self.move(self.row, self.column)
 
     def move(self, row, column):
         self.row = row
         self.column = column
-        x0 = (column * self.board.size) + int(self.board.size / 2)
-        y0 = (row * self.board.size) + int(self.board.size / 2)
-        self.board.canvas.coords(self.name, x0, y0)
+        self.board.canvas.coords(self.name, column, row)
 
 
 if __name__ == "__main__":
@@ -87,5 +93,7 @@ if __name__ == "__main__":
     p2_fire = Piece("two", "p2_fire", "fire", board)
     p2_water = Piece("two", "p2_water", "water", board)
     p2_earth = Piece("two", "p2_earth", "earth", board)
+
+    board.canvas.bind("<Button-1>", lambda event, arg=board: board.callback(event, arg))
 
     root.mainloop()
